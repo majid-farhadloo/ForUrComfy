147df0793427cf7f51408240368b3e46ee72e134
jPierosParra <zodi101@mail.fresnostate.edu>
2018-03-04 14:09:37 -0800

    New:Background/Restaurant/Logo

---
+++ app/src/main/java/com/example/majid/forurcomfy/OrderActivity.java
@@ -0,0 +1,26 @@
+package com.example.majid.forurcomfy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__order);
+        final Button bRestaurant = (Button) findViewById(R.id.bRestaurant);
+
+        bRestaurant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent OrderIntent = new Intent(OrderActivity.
+                        this,VeggiMenuActivty.class);
+                OrderActivity.this.startActivity(OrderIntent);
+             }
+        });
+    }
+}
--- app/src/main/java/com/example/majid/forurcomfy/UserAreaActivity.java
+++ app/src/main/java/com/example/majid/forurcomfy/UserAreaActivity.java
@@ -1,15 +1,36 @@
 package com.example.majid.forurcomfy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class UserAreaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final EditText Welcome = (EditText)findViewById(R.id.hello);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
-    }
-}
+        final EditText Welcome = (EditText)findViewById(R.id.hello);
+        final Button bOrder = (Button) findViewById(R.id.bOrder);
+        final Button bDeliveree = (Button) findViewById(R.id.bDeliveree);
+
+        bOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent OrderIntent = new Intent(UserAreaActivity.
+                        this,OrderActivity.class);
+                UserAreaActivity.this.startActivity(OrderIntent);
+
+
+            }
+        });
+
+
+        }
+        };
+
+
+
