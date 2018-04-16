14a34139a0505468d80baf853328d3d101cf9feb
Calvin Tey <calvinteytaisoon@gmail.com>
2018-04-16 03:15:38 -0700

    order schema

---
+++ backend_folder/project_152/lib/order.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+var orderSchema = new mongoose.Schema({
+  foodName : String,
+  price : Number,
+  quantity: Number,
+  currentUser: String,
+});
+
+var order = mongoose.model ('order',orderSchema);
+module.exports = order;
