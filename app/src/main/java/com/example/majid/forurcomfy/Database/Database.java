12ce778b59de682b069f2f6ec2d2ed6af73ad94e
jPierosParra <zodi101@mail.fresnostate.edu>
2018-04-22 15:29:55 -0700

    Changing DB ver

--- app/src/main/java/com/example/majid/forurcomfy/Database/Database.java
+++ app/src/main/java/com/example/majid/forurcomfy/Database/Database.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class Database extends SQLiteAssetHelper {
     private static final String DB_NAME = "eatdb.db";
-    private static final int DB_VER-"1";
+    private static final int DB_VER=1;
     public Database(Context context) {
         super(context, DB_NAME, null, DB_VER);
     }
